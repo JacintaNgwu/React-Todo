5af4c0b30a70e4d2c04a4df7ad6e63f362b93ca8
JacintaNgwu <jacintangwu9900@gmail.com>
2023-01-27 09:02:38 +0530

    Removed committed console.log

--- src/functionBased/components/TodoItem.js
+++ src/functionBased/components/TodoItem.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react"
+import React, { useState } from "react"
 import styles from "./TodoItem.module.css"
 
 const TodoItem = props => {
@@ -31,12 +31,6 @@ const TodoItem = props => {
   } else {
     editMode.display = "none"
   }
-
-  useEffect(() => {
-    return () => {
-      console.log("Cleaning up...")
-    }
-  }, [])
 
   return (
     <li className={styles.item}>
